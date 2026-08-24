65d2460f3fd49d90d00d6de8d222f0571791af39
jyotsna wadhwa <jyotsnaw15@gmail.com>
2026-08-25 01:03:27 +0530

    program cs open api

--- my-first-api-azure/Program.cs
+++ my-first-api-azure/Program.cs
@@ -1,22 +1,19 @@
-using my_first_api_azure.Services;
-
-var builder = WebApplication.CreateBuilder(args);
+ using my_first_api_azure.Services;
+ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddSingleton<ITasksManager, FakeTasksManager>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
